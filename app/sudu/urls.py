8750c83f839719278b0cb6e7ed92258d1f473fc4
Antoine Claval <antoine.claval@youknowwhat.com>
2019-12-04 23:24:49 -0600

    add Grappelilli

--- app/sudu/urls.py
+++ app/sudu/urls.py
@@ -34,6 +34,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
