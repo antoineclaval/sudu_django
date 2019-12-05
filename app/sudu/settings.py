ffcdcc19fd8de532136b57ddcf66df25df81956c
Antoine Claval <antoine.claval@gmail.com>
2019-12-04 23:24:49 -0600

    add Grappelilli

--- app/sudu/settings.py
+++ app/sudu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(","
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',  
     'django.contrib.auth',
     'django.contrib.contenttypes',
