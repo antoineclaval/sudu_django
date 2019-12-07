dd18f6753cb7316dfae91f821a77877e4a26a670
Antoine Claval <antoine.claval@youknowwhat.com>
2019-12-06 19:50:51 -0600

    enrich Festival model

--- app/cinema/models.py
+++ app/cinema/models.py
@@ -1,24 +1,30 @@
 from django.db import models
-#from django_countries.fields import CountryField
+from django_countries.fields import CountryField
 
 
 class Festival(models.Model):
     name = models.CharField(max_length=200)
- #   month_occurence = models.DateTimeField('Periode')
-  #  is_african = model.BooleanField(default=false)
-   # country = CountryField()
-    #current_year_date = models.DateTimeField('Current year date')
-    # deadline_date
-    # price
-    # fee
-    # has_rental_fee
-    # is_competitive
-    # comments
-    # support
-    # link
+    month_occurence = models.DateTimeField('Periode',blank=True, null=True)
+    is_african = models.BooleanField(default=False)
+    country = CountryField(blank=False, null=False, default="FR")
+    current_year_date = models.DateTimeField('Current year date', blank=True, null=True)
+    deadline_date = models.DateTimeField('Deadline', blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    #fee = models.DecimalField()
+    has_rental_fee = models.BooleanField(default=False)
+    is_competitive = models.BooleanField(default=False)
+    comments = models.CharField(max_length=500, blank=True, null=True)
+    support = models.CharField(max_length=200, blank=True, null=True)
+    link = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+    	return self.name
 
 
 
 class Film(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+    	return self.name
 
