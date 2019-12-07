7844844716126ab3aeaf8105196cbd6500f63e4d
Antoine Claval <antoine.claval@youknowwhat.com>
2019-12-06 20:04:00 -0600

    add admin for festival

--- app/cinema/admin.py
+++ app/cinema/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Festival
 from .models import Film
 
-admin.site.register(Festival)
-admin.site.register(Film)
+
+class FestivalAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+
+class FilmAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+admin.site.register(Festival, FestivalAdmin)
+admin.site.register(Film, FilmAdmin)
