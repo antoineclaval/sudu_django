a80eacc22f923008876336297ef557a5b918c49d
Antoine Claval <antoine.claval@youknowwhat.com>
2019-12-06 19:16:48 -0600

    add empty models for cinema objects

--- app/sudu/settings.py
+++ app/sudu/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(","
 
 INSTALLED_APPS = [
     'grappelli',
+    'cinema.apps.CinemaConfig',
     'django.contrib.admin',  
     'django.contrib.auth',
     'django.contrib.contenttypes',
