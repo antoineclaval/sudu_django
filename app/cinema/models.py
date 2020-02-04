fe767e460f12fa7c6ac3e0c76619aa338da8513a
Antoine Claval <antoine.claval@gmail.com>
2020-02-03 21:50:36 -0600

    add month dropdown instead of dateTime for festivals

--- app/cinema/models.py
+++ app/cinema/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django_countries.fields import CountryField
 from enum import Enum 
+from django.utils.dates import MONTHS
 import datetime
 
 class Festival(models.Model):
     name = models.CharField(max_length=200)
-    month_occurence = models.DateTimeField('Periode', blank=True, null=True)
+    month_occurence = models.PositiveSmallIntegerField(choices=MONTHS.items(), null=True, blank=True)
     is_african = models.BooleanField(default=False)
     country = CountryField(blank=False, null=False, default="FR")
-    current_year_date = models.DateTimeField('Current year date', blank=True, null=True)
-    deadline_date = models.DateTimeField('Deadline', blank=True, null=True)
+    current_year_date = models.DateField('Current year date', blank=True, null=True)
+    deadline_date = models.DateField('Deadline', blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
     # fee = models.DecimalField()
     has_rental_fee = models.BooleanField(default=False)
@@ -59,16 +60,16 @@ class ResponseChoice(Enum):
 MY_CHOICES = [('SELECTIONED', 'Selectionned'), ('REFUSED', 'Refused'), ('NO_RESPONSE','No response yet')]
 
 class Submission(models.Model):
-    dateSubmission = models.DateTimeField('Submission Date', blank=True, null=True)
+    dateSubmission = models.DateField('Submission Date', blank=True, null=True)
     film = models.ForeignKey(Film, on_delete=models.CASCADE) 
     festival = models.ForeignKey(Festival, on_delete=models.CASCADE) 
     response = models.CharField( default='NO_RESPONSE' , max_length=30, choices=MY_CHOICES)  
-    responseDate = models.DateTimeField('Response Date', blank=True, null=True)
+    responseDate = models.DateField('Response Date', blank=True, null=True)
     def __str__(self):
         return '{} / {}'.format(self.film.name,self.festival.name) 
 
 class Projection(models.Model): 
-    date = models.DateTimeField('Periode', blank=True, null=True)
+    date = models.DateField('Periode', blank=True, null=True)
     location = models.CharField(max_length=200)
     film = models.ForeignKey(Film, on_delete=models.CASCADE) 
     def __str__(self):
