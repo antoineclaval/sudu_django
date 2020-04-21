cf4282437dba8f518d9e0f40e64f2975fe71a48b
Antoine Claval <antoine.claval@gmail.com>
2020-04-20 22:03:55 -0500

    report by year/month view and implementation

--- app/cinema/urls.py
+++ app/cinema/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('festival/inscription/<int:year>/<int:month_id>/<int:film_id>/', views.inscriptionByMonthAndFilm, name='index'),
-    path('festival/inscription/<int:year>/<int:month_id>/<int:film_id>/docx/<lang>', views.docxReport, name='index')
+    path('reports/', views.index, name='index'),
+    path('reports/<int:year>/<int:month_id>/', views.byMonth, name='byMonth'),
+    path('reports/<int:year>/<int:month_id>/<int:film_id>/', views.inscriptionByMonthAndFilm, name='inscriptionView'),
+    path('reports/<int:year>/<int:month_id>/<int:film_id>/docx/<lang>', views.docxReport, name='docxDownload'),
+    path('reports/<int:year>/<int:month_id>/zip', views.generateZipReport, name='multiReport')
+
 ]
--- app/cinema/views.py
+++ app/cinema/views.py
@@ -15,13 +15,17 @@ from .models import ResponseChoice
 from docx import Document
 
 import os, locale
-from datetime import date, time
+from datetime import date
+import time
 import datetime
 from sudu.settings import BASE_DIR
 from babel.dates import format_date, format_datetime, format_time
 
 from io import StringIO
+from io import BytesIO
 from zipfile import ZipFile
+
+import calendar
 
 
 def image_upload(request):
@@ -36,40 +40,48 @@ def image_upload(request):
         })
     return render(request, "upload.html")
 
-#         zf.writestr(movieID, generateDocXReport(month,year, lang, movieID).save(iostream).getvalue())
-
-def generateReport(request):
+def generateZipReport(request, year, month_id):
     response = HttpResponse(content_type='application/zip')
     movie_id_list = request.GET.getlist('movie') 
-    month = request.GET['month']
-    year = request.GET['year']
-    lang = request.GET['lang']
-    print (movie_id_list)
-    print(   "/" + month + "/" + year + "/" )
+    # lang = request.GET['lang']
+    # print (movie_id_list)
   
-    in_memory = StringIO()
-    zip = ZipFile(in_memory, "a")
-        
-    zip.writestr("file1.txt", "some text contents")
-    zip.writestr("file2.csv", "csv,data,here")
+    in_memory_zip = BytesIO()
+    zip = ZipFile(in_memory_zip, "a")
+
+    for movieID in movie_id_list :
+        inMemoryDoc = BytesIO()
+        docxDoc = generateDocXReport(month_id,year, "fr", movieID)
+        docxDoc.save(inMemoryDoc)
+        zip.writestr(docxDoc.core_properties.title+".docx", inMemoryDoc.getvalue())
+
+        docxDoc = generateDocXReport(month_id,year, "en", movieID)
+        docxDoc.save(inMemoryDoc)
+        zip.writestr(docxDoc.core_properties.title+".docx", inMemoryDoc.getvalue())
     
     # fix for Linux zip files read in Windows
     for file in zip.filelist:
-        file.create_system = 0        
-    zip.close()
+        file.create_system = 0       
 
-    response = HttpResponse(mimetype="application/zip")
-    response["Content-Disposition"] = "attachment; filename=two_files.zip"
+    zip.close()
+    response["Content-Disposition"] = F'attachment; filename=Sudu-Report-{month_id}-{year}.zip'
     
-    in_memory.seek(0)    
-    response.write(in_memory.read())
+    in_memory_zip.seek(0)    
+    response.write(in_memory_zip.read())
     return response
 
 
 def index(request):
+    year, month_id = map(int, time.strftime("%Y %m").split())
+    byMonth(request,year, month_id)
+
+def byMonth(request, year, month_id):
     template = loader.get_template('index.html')
     context = {
-        'movies_list': Film.objects.all()
+        'movies_list': Film.objects.all(),
+        'current_month_name': calendar.month_name[month_id],
+        'current_year': time.strftime("%Y"),
+
     }
     return HttpResponse(template.render(context, request))
 
@@ -83,73 +95,14 @@ def inscriptionByMonthAndFilm(request, month_id, year, film_id):
 
 
 def docxReport(request, month_id, year, lang,film_id):
-
-    langMap = dict(fr = {'template': 'TEMPLATE _ Suivi de diffusion.docx', 'locale': 'fr_FR', 'emptyList' : 'Pas Encore.'},
-        en = {'template': 'TEMPLATE _ Distribution Tracking.docx', 'locale': 'en_US', 'emptyList' : 'Not yet.'})
-
-    locale.setlocale(locale.LC_TIME, langMap.get(lang).get('locale'))
-
-    file_path = os.path.join(BASE_DIR, 'temp/')
-    currentFilm =  Film.objects.get(id=film_id)
-    subList = Submission.objects.filter(dateSubmission__year=year).filter(dateSubmission__month=month_id).filter(film_id = film_id)
-
-    selectList = Submission.objects.filter(dateSubmission__year=year).filter(film_id = film_id).filter(response__iexact = 'SELECTIONED')
-    rejectList = Submission.objects.filter(dateSubmission__year=year).filter(film_id = film_id).filter(response__iexact = 'REFUSED')
-
-    subOutput, selectOutput, rejectOutput = "","",""
-
-    print("\nInscriptions:")
-    for item in subList:
-        print(item.festival.name + " (" +  item.festival.country.name +")")
-        subOutput += (item.festival.name + " (" +  item.festival.country.name +")\n") 
-    if not subOutput:
-        subOutput = langMap[lang]['emptyList']
-
-    print("\Selection:")
-    for item in selectList:
-        print(item.festival.name + " (" +  item.festival.country.name +")")
-        selectOutput += (item.festival.name + " (" +  item.festival.country.name +")\n") 
-    if not selectOutput:
-        selectOutput = langMap[lang]['emptyList']
-
-    print("\Rejection:")
-    for item in rejectList:
-        print(item.festival.name + " (" +  item.festival.country.name +")")
-        rejectOutput += (item.festival.name + " (" +  item.festival.country.name +")\n") 
-    if not rejectOutput:
-        rejectOutput = langMap[lang]['emptyList']
-
-
-
-    document = Document(file_path + langMap[lang]['template'])
-
-    dic = {'INSCRIPTIONS_LIST':subOutput,
-        'MOVIE_NAME':currentFilm.name, 
-        'CURRENT_DATE': format_datetime(date.today(), format='dd MMMM YYYY', locale=langMap[lang]['locale']),
-        'TARGET_MONTH': format_datetime(datetime.datetime(1900, int(month_id),1), format='MMMM',locale=langMap[lang]['locale']),
-        'TARGET_YEAR': str(year),
-        'SELECTIONS_LIST': selectOutput,
-        'REJECTIONS_LIST': rejectOutput,
-        'PROJECTIONS_LIST': langMap[lang]['emptyList']
-        }
-
-    for p in document.paragraphs:
-        pItems = p.text.split(" ")
-        for item in pItems:
-            item = item.strip()
-            if item in dic.keys():
-                p.text = p.text.replace(item,dic[item])
-
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    response['Content-Disposition'] = F'attachment; filename={currentFilm.name}-{month_id}-{year}-{lang}.docx'
+    document = generateDocXReport(month_id, year, lang, film_id)
     document.save(response)
+    response['Content-Disposition'] = F'attachment; filename={document.core_properties.title}-{month_id}-{year}-{lang}.docx'
     return response
 
 
-
-
-def generateDocXReport(month_id, year, lang,film_id):
-
+def generateDocXReport(month_id, year, lang, film_id):
     langMap = dict(fr = {'template': 'TEMPLATE _ Suivi de diffusion.docx', 'locale': 'fr_FR', 'emptyList' : 'Pas Encore.'},
         en = {'template': 'TEMPLATE _ Distribution Tracking.docx', 'locale': 'en_US', 'emptyList' : 'Not yet.'})
 
@@ -185,14 +138,12 @@ def generateDocXReport(month_id, year, lang,film_id):
     if not rejectOutput:
         rejectOutput = langMap[lang]['emptyList']
 
-
-
     document = Document(file_path + langMap[lang]['template'])
 
     dic = {'INSCRIPTIONS_LIST':subOutput,
-        'MOVIE_NAME':currentFilm.name, 
+        'MOVIE_NAME' : currentFilm.name, 
         'CURRENT_DATE': format_datetime(date.today(), format='dd MMMM YYYY', locale=langMap[lang]['locale']),
-        'TARGET_MONTH': format_datetime(datetime.datetime(1900, int(month_id,1), format='MMMM',locale=langMap[lang]['locale'])),
+        'TARGET_MONTH': format_datetime(datetime.datetime(1900, int(month_id) ,1), format='MMMM',locale=langMap[lang]['locale']),
         'TARGET_YEAR': str(year),
         'SELECTIONS_LIST': selectOutput,
         'REJECTIONS_LIST': rejectOutput,
@@ -203,10 +154,8 @@ def generateDocXReport(month_id, year, lang,film_id):
         pItems = p.text.split(" ")
         for item in pItems:
             item = item.strip()
+            print(item)
             if item in dic.keys():
                 p.text = p.text.replace(item,dic[item])
-
-    #response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    #response['Content-Disposition'] = F'attachment; filename={currentFilm.name}-{month_id}-{year}-{lang}.docx'
-# document.save(response)
+    document.core_properties.title = currentFilm.name+"-"+lang
     return document
