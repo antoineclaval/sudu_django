2efce11a216147d19907cae8fc3d8ae4f8a86336
Antoine Claval <antoine.claval@eyeonsurveillance.org>
2026-04-19 17:22:42 -0500

    I should get a linter

--- app/sudu/models.py
+++ app/sudu/models.py
@@ -18,28 +18,34 @@ class Language(models.Model):
     is not needed.
     This should be popluated by getting data from django.conf.locale.LANG_INFO
     '''
-    name = models.CharField(max_length=256,
-                            null=False,
-                            blank=False,
-                            verbose_name='Language name'
-                            )
-    name_local = models.CharField(max_length=256,
-                                null=False,
-                                blank=True,
-                                default='',
-                                verbose_name='Language name (in that language)')
-    isocode = models.CharField(max_length=2,
-                            null=False,
-                            blank=False,
-                            unique=True,
-                            verbose_name='ISO 639-1 Language code',
-                            help_text='2 character language code without country'
-                            )
-    sorting = models.PositiveIntegerField(blank=False,
-                                        null=False,
-                                        default=0,
-                                        verbose_name='sorting order',
-                                        help_text='increase to show at top of the list')
+    name = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False,
+        verbose_name='Language name',
+    )
+    name_local = models.CharField(
+        max_length=256,
+        null=False,
+        blank=True,
+        default='',
+        verbose_name='Language name (in that language)',
+    )
+    isocode = models.CharField(
+        max_length=2,
+        null=False,
+        blank=False,
+        unique=True,
+        verbose_name='ISO 639-1 Language code',
+        help_text='2 character language code without country',
+    )
+    sorting = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+        default=0,
+        verbose_name='sorting order',
+        help_text='increase to show at top of the list',
+    )
 
     def __str__(self):
         return '%s (%s)' % (self.name, self.name_local)
