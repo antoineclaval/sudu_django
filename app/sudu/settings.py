c4e2a8418d7cce1aef31256191d5b9a1bc71486d
Antoine Claval <antoine.claval@eyeonsurveillance.org>
2026-04-19 21:53:41 -0500

    an actual logging policy

--- app/sudu/settings.py
+++ app/sudu/settings.py
@@ -120,3 +120,19 @@ MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+    },
+}
